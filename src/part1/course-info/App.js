4149ee396d90a97c8b0647d74780a1d943d0123b
mohamedisakr <mohamedisakr@gmail.com>
2021-09-28 16:22:18 +0200

    implementing exercise 1.3

--- src/part1/course-info/App.js
+++ src/part1/course-info/App.js
@@ -5,6 +5,20 @@ import Total from './Total'
 
 const App = () => {
   const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  }
+  /*
+  const course = 'Half Stack application development'
 
   const exercises = [10, 7, 14]
 
@@ -13,10 +27,11 @@ const App = () => {
     {name: 'Using props to pass data', exercises: 7},
     {name: 'State of a component', exercises: 14},
   ]
-
+  */
+  const parts = [part1, part2, part3]
+  const exercises = parts.map(({exercises}) => exercises)
   return (
     <div>
-      <h1>{course}</h1>
       <Header name={course} />
       <Content parts={parts} />
       <Total exercises={exercises} />
