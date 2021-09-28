317a1bee425db9a6e2b63a49a6c68e1a51e14ef6
mohamedisakr <mohamedisakr@gmail.com>
2021-09-28 13:53:59 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+const Hello = (props) => {
+  const {name, age} = props
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hello {name}, you are {age} years old
       </header>
     </div>
-  );
+  )
+}
+const App = () => {
+  const name = 'el 3afreet el 2ara3'
+  const age = '44'
+  return (
+    <div>
+      <Hello name={name} age={age} />
+    </div>
+  )
 }
 
-export default App;
+export default App
+
+{
+  /* <img src={logo} className="App-logo" alt="logo" />
+<p>
+  Edit <code>src/App.js</code> and save to reload.
+</p>
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a> */
+}
