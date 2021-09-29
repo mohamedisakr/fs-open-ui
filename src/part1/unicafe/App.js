64c89946827fb7bcacec4395084a57c036929104
mohamedisakr <mohamedisakr@gmail.com>
2021-09-29 07:47:57 +0200

    implementing exercise 1.7 unicafe step2

--- src/part1/unicafe/App.js
+++ src/part1/unicafe/App.js
@@ -5,17 +5,25 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const positive = (good / total) * 100
+  // good: 1, neutral: 0, bad: -1
+  const average = (good * 1 + neutral * 0 + bad * -1) / total
 
   const handleGood = (e) => {
     setGood(good + 1)
+    setTotal(total + 1)
   }
 
   const handleNeutral = (e) => {
     setNeutral(neutral + 1)
+    setTotal(total + 1)
   }
 
   const handleBad = (e) => {
     setBad(bad + 1)
+    setTotal(total + 1)
   }
 
   return (
@@ -37,6 +45,9 @@ const App = () => {
         <p>good: {good}</p>
         <p>neutral: {neutral}</p>
         <p>bad: {bad}</p>
+        <p>all: {total}</p>
+        <p>average: {average}</p>
+        <p>positive: {positive} %</p>
       </div>
     </>
   )
