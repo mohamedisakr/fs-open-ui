b418626b59f66517256437923d928d75187eff86
mohamedisakr <mohamedisakr@gmail.com>
2021-09-30 13:12:38 +0200

    implementing exercise 2.9* The Phonebook Step4

--- src/part2/phonebook/App.js
+++ src/part2/phonebook/App.js
@@ -5,8 +5,9 @@ const App = () => {
   const [persons, setPersons] = useState(payload)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNUmber] = useState('')
+  const [keyword, setKeyword] = useState('')
 
-  const resetState = () => {
+  const resetControls = () => {
     setNewName('')
     setNewNUmber('')
   }
@@ -37,18 +38,36 @@ const App = () => {
     const isExist = preventAddDuplicateName(personToAdd.name)
     if (isExist === true) {
       alert(`${newName} is already added to phonebook`)
-      resetState()
+      resetControls()
       return
     }
 
     setPersons(persons.concat(personToAdd))
-    resetState()
+    resetControls()
   }
+
+  const updateSearch = (query) => {
+    setKeyword(query.trim())
+  }
+
+  const personsToShow =
+    keyword === ''
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(keyword.toLowerCase()),
+        )
 
   return (
     <div>
       <h1>Phonebook</h1>
-      <h2>Names</h2>
+      <div>
+        filter:
+        <input
+          value={keyword}
+          onChange={(event) => updateSearch(event.target.value)}
+        />
+      </div>
+      <h2>Add New Person</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -60,8 +79,9 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+      <h2>Names</h2>
       <ul>
-        {persons.map((person) => {
+        {personsToShow.map((person) => {
           const {id, name, number} = person
           return (
             <li key={id}>
