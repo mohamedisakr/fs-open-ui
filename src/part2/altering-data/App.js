2e98244d79ac213cb093cd5df075ce6aa5d3978c
mohamedisakr <mohamedisakr@gmail.com>
2021-10-02 09:28:26 +0200

    adding styles to react app

--- src/part2/altering-data/App.js
+++ src/part2/altering-data/App.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import {getAll, create, update} from '../../services/notes'
 import Note from './Note'
+import Notification from '../../utils/Notification'
+import Footer from './Footer'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   const hook = () => {
     getAll().then((initialNotes) => {
@@ -46,7 +49,12 @@ const App = () => {
         setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))
       })
       .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`)
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`,
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter((n) => n.id !== id))
       })
   }
@@ -63,7 +71,7 @@ const App = () => {
           show {showAll ? 'important' : 'all'}
         </button>
       </div>
-      <ul>
+      <ul className="note-list">
         {notesToShow.map((note) => {
           let {id, content, important} = note
           return (
@@ -80,6 +88,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
--- src/part2/altering-data/Note.js
+++ src/part2/altering-data/Note.js
@@ -1,7 +1,7 @@
 const Note = ({id, content, important, toggleImportance}) => {
   const label = important ? 'make no important' : 'make important'
   return (
-    <li key={id}>
+    <li key={id} className="note">
       {content} <button onClick={toggleImportance}>{label}</button>
     </li>
   )
