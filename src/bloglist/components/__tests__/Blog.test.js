5a943dac2bd6dd005d6864bfa35e8cebbff80284
mohamedisakr <mohamedisakr@gmail.com>
2021-10-15 07:27:58 +0200

    debugging tests

--- src/bloglist/components/__tests__/Blog.test.js
+++ src/bloglist/components/__tests__/Blog.test.js
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom/extend-expect'
 import {render} from '@testing-library/react'
+import {prettyDOM} from '@testing-library/dom'
 import Blog from '../Blog'
 
 test('renders title', () => {
@@ -19,6 +20,20 @@ test('renders title - method 2', () => {
     user: {name: 'Mohamed Sakr'},
   }
   const component = render(<Blog blog={blog} />)
+  component.debug()
   const element = component.getByText(/React Cookbook/i)
   expect(element).toBeDefined()
+})
+
+test('renders content', () => {
+  const blog = {
+    id: '61660824ff82642821bb6c3c',
+    title: 'React Cookbook',
+    user: {name: 'Mohamed Sakr'},
+  }
+
+  const component = render(<Blog blog={blog} />)
+  const li = component.container.querySelector('li')
+
+  console.log(prettyDOM(li))
 })
