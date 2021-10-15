991854b5337da9dec52a2f5bf5cdd4f81ed3f318
mohamedisakr <mohamedisakr@gmail.com>
2021-10-15 07:40:21 +0200

    clicking buttons in tests

---
+++ src/part2/altering-data/__tests__/Note.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import {prettyDOM} from '@testing-library/dom'
+import Note from '../Note'
+
+test('clicking the button calls event handler once', () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true,
+  }
+
+  const mockHandler = jest.fn()
+
+  const component = render(<Note note={note} toggleImportance={mockHandler} />)
+
+  const button = component.getByText(/make important/i)
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
+})
