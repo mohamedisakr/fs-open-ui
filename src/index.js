b5eb84deb8f99d3cfecfe8e9f65d8e3deb4bea3b
mohamedisakr <mohamedisakr@gmail.com>
2021-10-18 13:42:18 +0200

    refactoring app file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+// import {createStore} from 'redux'
+// import {Provider} from 'react-redux'
 import App from './part06/unicafe-redux/App'
+// import counterReducer from './part06/unicafe-redux/reducers/counterReducer'
+
+// const store = createStore(counterReducer)
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <Provider store={store}> */}
     <App />
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root'),
 )
