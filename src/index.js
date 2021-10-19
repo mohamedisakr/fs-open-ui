6f52e520475bee7052e4b263a1e9901546305864
mohamedisakr <mohamedisakr@gmail.com>
2021-10-19 13:24:49 +0200

    using redux devtools

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import {createStore, combineReducers} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import {Provider} from 'react-redux'
 import App from './part06-many-reds/note-app/App'
 import noteReducer from './part06-many-reds/note-app/reducers/noteReducer'
 import filterReducer from './part06-many-reds/note-app/reducers/filterReducer'
 
 const reducer = combineReducers({notes: noteReducer, filter: filterReducer})
-const store = createStore(reducer)
+const store = createStore(reducer, composeWithDevTools())
 console.log(store.getState())
 
 ReactDOM.render(
