18eea274bb109b5925e5cacf6721fa6d6ee044df
mohamedisakr <mohamedisakr@gmail.com>
2022-01-17 08:55:05 +0200

    implementing pagination structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import './App.css'
 import CustomerList from './pagination-customers/CustomerList'
 
-// const Hello = (props) => {
-//   const {name, age} = props
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         Hello {name}, you are {age} years old
-//       </header>
-//     </div>
-//   )
-// }
 const App = () => {
-  // const name = 'el 3afreet el 2ara3'
-  // const age = '44'
   return (
     <div>
       {/* <Hello name={name} age={age} /> */}
--- src/pagination-customers/CustomerList.js
+++ src/pagination-customers/CustomerList.js
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react'
+import ReactPaginate from 'react-paginate'
 import {getAll} from '../services/customers'
 
 const CustomerList = () => {
   const [customers, setCustomers] = useState([])
   const [meta, setMeta] = useState({})
+  const [pageNumber, setPageNumber] = useState(1)
 
   useEffect(() => {
     loadAllCustomers()
@@ -11,19 +13,47 @@ const CustomerList = () => {
 
   const loadAllCustomers = async () => {
     const res = await getAll()
-    console.log(`response : ${JSON.stringify(res, null, 4)}`)
-    // setCustomers(res)
+    // console.log(`response : ${JSON.stringify(res, null, 4)}`)
+
     const {meta, results} = res.data
     setMeta(meta)
     setCustomers(results)
+  }
+
+  const {total, pages, next, previous} = meta
+
+  const displayCustomers = () => {
+    return customers.map(({_id, username, name, email}) => {
+      return (
+        <div key={_id}>
+          <p>{username}</p>
+          <p>{name}</p>
+          <p>{email}</p>
+        </div>
+      )
+    })
+  }
+
+  const handlePageChange = ({selected}) => {
+    setPageNumber(selected)
   }
 
   return (
     <div>
       {customers && meta && (
         <>
-          <pre>{JSON.stringify(meta, null, 4)}</pre>
-          <pre>{JSON.stringify(customers, null, 4)}</pre>
+          {displayCustomers()}
+          <ReactPaginate
+            pageCount={pages}
+            onPageChange={handlePageChange}
+            previousLabel={'Previous'}
+            nextLabel={'Next'}
+            containerClassName={'paginationBttns'}
+            previousLinkClassName={'previousBttn'}
+            nextLinkClassName={'nextBttn'}
+            disabledClassName={'paginationDisabled'}
+            activeClassName={'paginationActive'}
+          />
         </>
       )}
     </div>
@@ -32,15 +62,7 @@ const CustomerList = () => {
 
 export default CustomerList
 
-/*
-  customers &&
-        customers.data &&
-        customers.data.meta &&
-        customers.data.results
-        && (
-        <>
-          <pre>{JSON.stringify(customers.data.meta, null, 4)}</pre>
-          <pre>{JSON.stringify(customers.data.results, null, 4)}</pre>
-        </>
-      )
-  */
+/* 
+<pre>{JSON.stringify(meta, null, 4)}</pre>
+<pre>{JSON.stringify(customers, null, 4)}</pre> 
+*/
